2ae9d64afb111eeeeb1a16e40b305691a53cde13
Beatriz Salles <beatrizsallesss@gmail.com>
2022-10-17 22:10:03 -0300

    fix: 🐛 Close dropdown when click  outside it

--- src/pages/Home/components/Dropdown/index.jsx
+++ src/pages/Home/components/Dropdown/index.jsx
@@ -1,32 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 import './styles.css';
 
+const options = [
+  {
+    id: 0,
+    value: 'Administração',
+  },
+  {
+    id: 1,
+    value: 'Estoque',
+  },
+  {
+    id: 2,
+    value: 'Vendas',
+  },
+];
+
 export default function Dropdown() {
-  const options = [
-    {
-      id: 0,
-      value: 'Administração',
-    },
-    {
-      id: 1,
-      value: 'Estoque',
-    },
-    {
-      id: 2,
-      value: 'Vendas',
-    },
-  ];
   const [sector, setSector] = useState('Selecione uma ocupação');
   const [isOpen, setIsOpen] = useState(false);
+  const divRef = useRef(null);
 
   const onSelectOption = option => {
     setSector(option.value);
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const checkIfClickedOutside = evt => {
+      if (divRef.current && !divRef.current.contains(evt.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', checkIfClickedOutside);
+    return () => {
+      document.removeEventListener('mousedown', checkIfClickedOutside);
+    };
+  }, []);
+
   return (
-    <div className="dropdown">
+    <div ref={divRef} className="dropdown">
       <div
         className="dropdown-container"
         onClick={() => setIsOpen(prev => !prev)}>
